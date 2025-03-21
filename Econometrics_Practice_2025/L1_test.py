4a92ebeb2e038dfa419fed95e48a52643ec8f2e4
jiqingxxm <jiqingxxm@163.com>
2025-03-21 19:57:30 +0800

    update

--- Econometrics_Practice_2025/L1_test.py
+++ Econometrics_Practice_2025/L1_test.py
@@ -1,4 +1,9 @@
 import sys
+import platform
 
-print(" Python版本信息: ", sys.version)
-print(" 当前Python环境路径: ", sys.executable)
+print("-" * 50)
+print(f"Python 版本信息: {sys.version}")
+print(f"Python 环境路径: {sys.executable}")
+print(f"操作系统信息: {platform.platform()}")
+print(f"Python 实现方式: {platform.python_implementation()}")
+print("-" * 50)
